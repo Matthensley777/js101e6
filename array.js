a41ba9fdd6128a4f3325d9e2be29b080e8783d5f
Matt Hensley <matt.hensley777@gmail.com>
2017-07-25 19:51:29 -0500

    colors and arrays and loops

---
+++ array.js
@@ -0,0 +1,74 @@
+// console.log("hello");
+
+// var pokemon = "charmander";
+
+// var array = [42, "string", null, "car", {}, pokemon];
+
+// console.log(array.indexOf("car"));
+
+// // array methods
+
+// array.push("cat");
+// console.log(array);
+
+// array.pop();
+// console.log(array);
+
+
+// //adds to beginning
+// array.unshift("zoe is cool");
+// console.log(array);
+
+// //removes from beginning
+// array.shift();
+// console.log(array);
+
+// var newString = array.join("|");
+// console.log(newString);
+
+// newString.split("|");
+// console.log(array);
+
+// var sentence = "http://www.meetup.com/"
+// console.log(sentence.split("/"));
+// console.log(sentence.split(""));
+
+// var numberArray = [3, 6, 12, 55, 8]
+
+// // console.log(newArray.reverse());
+// // console.log(newArray.sort());
+// console.log(numberArray.sort());
+
+// console.log(numberArray.sort(function(first, second) {
+//     return first - second;
+// }));
+
+// // var result = newArray.slice(3, 2);
+// // console.log(result);
+// console.log(numberArray);
+// console.log(numberArray[0]);
+
+
+// for loops
+var newArray = ["dog", "cat", "pig", "hippo"];
+
+for (var i = 0;i < newArray.length; i+=1) {
+	debugger;
+	console.log("i'm at index" + i);
+	console.log(array[i]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
